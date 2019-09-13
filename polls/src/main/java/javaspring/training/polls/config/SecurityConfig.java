32d0314a95b9dcccc8afa49c02f7867772160970
Huynh Vu <vdhuynh@tma.com.vn>
2019-09-14 00:04:57 +0700

    update the Security Config

--- polls/src/main/java/javaspring/training/polls/config/SecurityConfig.java
+++ polls/src/main/java/javaspring/training/polls/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-				.sessionManagement().sessionGreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequest()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
 				.antMatchers("/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html",
 						"/**/*.css", "/**/*.js")
 				.permitAll().antMatchers("/api/auth/**").permitAll()
