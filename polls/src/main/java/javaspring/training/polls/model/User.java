51b9d675d995dd430b0bbb256266bc304c27dda0
Huynh Vu <vdhuynh@tma.com.vn>
2019-09-14 08:49:21 +0700

    Authentication Controller

--- polls/src/main/java/javaspring/training/polls/model/User.java
+++ polls/src/main/java/javaspring/training/polls/model/User.java
@@ -52,11 +52,10 @@ public class User extends DateAudit {
 
 	}
 
-	public User(Long id, String name, String username, String password) {
-		super();
-		this.id = id;
+	public User(String name, String username, String email, String password) {
 		this.name = name;
 		this.username = username;
+		this.email = email;
 		this.password = password;
 	}
 
