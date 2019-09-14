7ed508aa5a3ce7f716382f897d72a404ff54a10c
Huynh Vu <vdhuynh@tma.com.vn>
2019-09-14 08:48:49 +0700

    Custom Business Exceptions #2

--- polls/src/main/java/javaspring/training/polls/exception/BadRequestException.java
+++ polls/src/main/java/javaspring/training/polls/exception/BadRequestException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
+	
 	public BadRequestException(String message) {
 		super(message);
 	}
--- polls/src/main/java/javaspring/training/polls/exception/ResourceNotFoundException.java
+++ polls/src/main/java/javaspring/training/polls/exception/ResourceNotFoundException.java
@@ -1,6 +1,12 @@
 package javaspring.training.polls.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@SuppressWarnings("serial")
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+	
 	private String resourceName;
     private String fieldName;
     private Object fieldValue;
